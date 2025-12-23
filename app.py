e1629bdcc7c2c6c75bd49daad5d59c41cf416fc1
Hubery <hubery8065@hotmail.com>
2025-12-23 21:19:28 +0800

    understand tasks.html and make it simpler

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("tasks.html")
+    quadrants = [
+        {"id": 1, "title": "重要且紧急", "btn_class": "btn-danger", "q_class": "q1"},
+        {"id": 2, "title": "重要不紧急", "btn_class": "btn-primary", "q_class": "q2"},
+        {"id": 3, "title": "不重要但紧急", "btn_class": "btn-warning", "q_class": "q3"},
+        {"id": 4, "title": "不重要不紧急", "btn_class": "btn-success", "q_class": "q4"},
+    ]
+    return render_template("tasks.html", quadrants=quadrants)
